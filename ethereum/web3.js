82bc418b2f656fd0700057ac3de4576cd586c450
Taras Shulhan <taras.shulhan@gmail.com>
2022-06-08 13:40:20 -0400

    refractored to avoid using custom server to allow for easy deployment

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -6,12 +6,14 @@ if (typeof window !== "undefined" && typeof window.ethereum !== "undefined") {
   // We are in the browser and metamask is running.
   window.ethereum.request({ method: "eth_requestAccounts" })
   web3 = new Web3(window.ethereum)
+  console.log("meta")
 } else {
   // We are on the server *OR* the user is not running metamask
   const provider = new Web3.providers.HttpProvider(
     "https://rinkeby.infura.io/v3/9fe88971851d4fb1b8501ed1a22abfbd"
   )
   web3 = new Web3(provider)
+  console.log("no meta")
 }
 
 export default web3
